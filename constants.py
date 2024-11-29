d45ea08dba853cae77652e25c3f2ffaf1c001b10
Aleksandr Kazakov <emtyxd@mail.ru>
2024-11-29 17:07:00 +0300

    unload model

---
+++ constants.py
@@ -0,0 +1,8 @@
+MODEL_PATHS = {
+    "villager": "models/villager/model.pth",
+    "житель": "models/villager/model.pth",
+    "leavsy": "models/leavsy/model.pth",
+    "ливси": "models/leavsy/model.pth"
+}
+
+AUDIO_PATH = "audio_files"
--- main.py
+++ main.py
@@ -4,30 +4,25 @@ from flask import Flask, after_this_request, request, send_file
 import pyttsx3
 import uuid
 from rvc_python.infer import RVCInference
+from constants import MODEL_PATHS, AUDIO_PATH
 
 
 tts = pyttsx3.init()
 rvc = RVCInference()
 app = Flask(__name__)
 
-model_paths = {
-    "villager": "models/villager/model.pth",
-    "житель": "models/villager/model.pth",
-    "leavsy": "models/leavsy/model.pth",
-    "ливси": "models/leavsy/model.pth"
-}
-
 def text_to_speech(text: str) -> str:
-    makedirs("audio_files", exist_ok=True)
-    output_path = path.join("audio_files", f"{uuid.uuid4()}.mp3")
+    makedirs(AUDIO_PATH, exist_ok=True)
+    output_path = path.join(AUDIO_PATH, f"{uuid.uuid4()}.mp3")
     engine = pyttsx3.init()
     engine.save_to_file(text, output_path)
     engine.runAndWait()
     return output_path
 
-def clone_voice(input_path: str, output_path: str, model_path: str):
+def clone_voice(input_path: str, output_path: str, model_path: str) -> str:
     rvc.load_model(model_path)
     rvc.infer_file(input_path, output_path)
+    rvc.unload_model()
     return output_path
 
 @app.route('/synthesize', methods=['POST'])
@@ -36,15 +31,15 @@ def synthesize():
 
     text = data['text']
 
-    model = data.get('model', 'villager')  # Здесь 'villager' — это дефолтное значение
-    if model not in model_paths:
-        return {"message": f"Модель '{model}' не найдена."}, 400  # Возвращаем ошибку, если модель не существует
-    model = model_paths[data['model']]
+    model = data.get('model', 'villager')
+    if model not in MODEL_PATHS:
+        return {"message": f"Модель '{model}' не найдена."}, 400
+    model = MODEL_PATHS[data['model']]
 
     audio_file = text_to_speech(text)
     processed_voice = clone_voice(
         audio_file,
-        f"audio_files/{uuid.uuid4()}.mp3",
+        path.join(AUDIO_PATH, f"{uuid.uuid4()}.mp3"),
         model
         )
     
